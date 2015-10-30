67bc03d660ee23c0c640bd8597f1c2695512a242
yangjunbao <acrazing@gmail.com>
2015-10-30 23:55:27 +0800

    init

---
+++ template/template.js
@@ -0,0 +1,11 @@
+/**
+ * @file ${FILE_NAME}. Created by PhpStorm.
+ * @desc ${FILE_NAME}.
+ *
+ * @author yangjunbao
+ * @since 15/10/29 上午10:31
+ * @version 1.0.0
+ */
+// 页面js文件
+$(function () {
+});
