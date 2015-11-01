6dd0e7adfd07393b3c42f1c70f5d1ee9ca6dd5c9
yangjunbao <acrazing@gmail.com>
2015-11-01 23:04:09 +0800

    food item page

--- food_item/food_item.js
+++ food_item/food_item.js
@@ -8,4 +8,14 @@
  */
 // 页面js文件
 $(function () {
+    var $mAddCart = $('#m-add-cart'),
+        foodId = $mAddCart.data('id'),
+        $num = $mAddCart.find('.food-num-val');
+    $mAddCart.on('click', 'input', function () {
+        _.addCart(foodId, +$num.html());
+    }).on(_.touchEnd, '.icon-subtract', function () {
+        $num.html(+$num.html() - 1 || 1);
+    }).on(_.touchEnd, '.icon-add', function () {
+        $num.html(+$num.html() + 1);
+    })
 });
