76e6c087b9c1c68aa96467e615bf19568c1d3389
yangjunbao <acrazing@gmail.com>
2015-11-06 10:35:31 +0800

    watcher

--- watcher.go
+++ watcher.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
     path := "git@acrazing.me:/var/website/work/qingguo/10-29/"
+
     watcher, err := fsnotify.NewWatcher()
     if err != nil {
         log.Fatal(err)
@@ -23,34 +24,32 @@ func main() {
         for {
             select {
             case event := <-watcher.Events:
-                log.Println("event:", event)
-                if strings.HasSuffix(event.Name, "_") {
+                if strings.HasSuffix(event.Name, "_") || event.Op == fsnotify.Chmod {
                     continue
                 }
+            // log.Println("event:", event)
                 stat, err := os.Stat(event.Name)
                 if err != nil {
-                    log.Println("get file state error:", err)
+                    log.Println("get file status error:", err)
                     continue
                 }
                 if stat.IsDir() {
                     watcher.Add(event.Name)
                 }
-                if event.Op & fsnotify.Write == 0 && event.Op & fsnotify.Rename == 0 {
-                    continue
-                }
-                args := []string{"-vq"}
+                args := []string{"scp", "-vq"}
                 if stat.IsDir() {
-                    args[0] += "r"
+                    args[1] += "r"
+                } else if event.Op & fsnotify.Write == 0 && event.Op & fsnotify.Rename == 0 {
+                    continue
                 }
                 args = append(args, event.Name)
                 args = append(args, path + event.Name)
-                cmd := exec.Command("scp", args...)
-                out, err := cmd.Output()
+                cmd := exec.Command(args[0], args[1:]...)
+                _, err = cmd.Output()
                 if err != nil {
                     log.Println("scp error:", err)
                 } else {
-                    log.Println("scp", args[0], args[1], args[2])
-                    log.Println("output:", string(out))
+                    log.Println(args)
                 }
             case err := <-watcher.Errors:
                 log.Println("error:", err)
