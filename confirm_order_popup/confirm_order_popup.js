4e967c97eafce2cf3ee1b853ae9154c98f62847f
yangjunbao <acrazing@gmail.com>
2015-11-07 17:29:43 +0800

    scroller

--- confirm_order_popup/confirm_order_popup.js
+++ confirm_order_popup/confirm_order_popup.js
@@ -15,18 +15,18 @@ $(function () {
         startHour = date.getHours(),
         onClock = 7,
         offClock = 22,
+        year = startYear,
+        month = startMonth,
+        day = startDay,
+        max,
+        disabledClock = [],
+        dateLabels = ['今天', '明天', '后天'],
         config = {
             columns: [{
-                id: 'year',
+                id: 'date',
                 items: []
             }, {
-                id: 'month',
-                items: []
-            }, {
-                id: 'day',
-                items: []
-            }, {
-                id: 'clock',
+                id: 'time',
                 items: []
             }],
             position: 'middle',
@@ -35,18 +35,29 @@ $(function () {
             showFocusBorder: true,
             onChange: function (id, value, column, index) {
                 switch (column) {
-                    case 0: // change year
+                    case 0: // change date
+                        if (index === 0) {
+                            this.disable(1, disabledClock)
+                        } else {
+                            this.enable(1, disabledClock)
+                        }
                         break;
-                    case 1: // change month
-                        break;
-                    case 2: // change date
-                        break;
-                    case 3: // change clock
+                    case 1: // change time
+                        if(disabledClock.indexOf(index) === -1) {
+                            this.enable(0, 0)
+                        } else {
+                            this.disable(0, 0)
+                        }
                         break;
                 }
             },
             confirmCallback: function (values) {
-                $('#m-result').find('.result').html(values.join('-'));
+                values.forEach(function(d) {
+                    d.forEach(function(v, i) {
+                        d[i] = _.lpad(v, 2, '0')
+                    })
+                });
+                $('#m-result').find('.result').html(values[0].join('-') + ' ' + values[1].join(':'));
             }
         },
         i;
@@ -57,32 +68,29 @@ $(function () {
             : (bigs.indexOf(month) === -1 ? 30 : 31);
     }
 
-    for (i = startYear - 5; i < startYear + 5; i++) {
+    for (i = 0; i < 30; i++) {
+        max = maxDay(year, month);
+        if (day + 1 > max) {
+            year = month === 12 ? year + 1 : year;
+            month = month === 12 ? 1 : month + 1;
+            day = 1
+        } else {
+            day += 1;
+        }
+
         config.columns[0].items.push({
-            value: i,
-            label: i,
-            disabled: false//i < startYear
+            value: [year, month, day],
+            label: dateLabels[i] || (month + '月' + day + '日'),
+            disabled: false
         })
     }
-    for (i = 1; i <= 12; i++) {
-        config.columns[1].items.push({
-            value: i,
-            label: i,
-            disabled: false//i < startMonth
-        })
-    }
-    for (i = 1; i <= 31; i++) {
-        config.columns[2].items.push({
-            value: i,
-            label: i,
-            disabled: false//i < startDay || i > maxDay(startYear, startMonth)
-        })
-    }
+
     for (i = onClock; i <= offClock; i++) {
-        config.columns[3].items.push({
-            value: i + ':00',
-            label: i + ':00',
-            disabled: false//i < startHour
+        startHour > i && disabledClock.push(i - onClock);
+        config.columns[1].items.push({
+            value: [i, 0, 0],
+            label: _.lpad(i, 2, '0') + ':00',
+            disabled: startHour > i
         })
     }
     var scroller = new _.Scroller(config);
